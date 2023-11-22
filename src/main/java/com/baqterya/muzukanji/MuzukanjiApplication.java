ac3598dc271109c48d5b5e5bdb745894278a7bcd
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-22 19:16:33 +0100

    Initial commit - spring project initialisation

---
+++ src/main/java/com/baqterya/muzukanji/MuzukanjiApplication.java
@@ -0,0 +1,13 @@
+package com.baqterya.muzukanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzukanjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzukanjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baqterya/muzukanji/MuzukanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baqterya.muzukanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzukanjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
