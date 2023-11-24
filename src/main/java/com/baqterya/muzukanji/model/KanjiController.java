2bb70115c251a3f84a3e478e74a71501a704aed9
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-24 16:35:31 +0100

    added missing getmapping annotation

--- src/main/java/com/baqterya/muzukanji/model/KanjiController.java
+++ src/main/java/com/baqterya/muzukanji/model/KanjiController.java
@@ -1,6 +1,7 @@
 package com.baqterya.muzukanji.model;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,9 @@ public class KanjiController {
 
     private final KanjiService kanjiService;
 
+
+
+    @GetMapping
     public List<Kanji> getAllKanji() {
         return kanjiService.getAllKanji();
     }
