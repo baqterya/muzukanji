97495ac4bfd88c1deb84136075ae024f08ce03bf
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-24 18:09:11 +0100

    created basic getAll test

--- src/test/java/com/baqterya/muzukanji/MuzukanjiApplicationTests.java
+++ src/test/java/com/baqterya/muzukanji/MuzukanjiApplicationTests.java
@@ -1,13 +1,98 @@
 package com.baqterya.muzukanji;
 
+import com.baqterya.muzukanji.model.Kanji;
+import com.baqterya.muzukanji.model.KanjiRepository;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MuzukanjiApplicationTests {
 
+	@LocalServerPort
+	private Integer port;
+
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:alpine");
+
+	@BeforeAll
+	static void beforeAll() {
+		postgres.start();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		postgres.stop();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
+
+	@Autowired
+	KanjiRepository kanjiRepository;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost:" + port;
+		kanjiRepository.deleteAll();
+	}
+
 	@Test
-	void contextLoads() {
+	void kanjiListLoads() {
+		List<Kanji> testData = List.of(
+				Kanji.builder()
+						.kanji("人")
+						.meanings(List.of("person"))
+						.kunyomi(List.of("ひと", "-り", "-と"))
+						.kunyomi_romaji(List.of("hito", "-ri", "-to"))
+						.onyomi(List.of("ジン", "ニン"))
+						.onyomi_romaji(List.of("jin", "nin"))
+						.strokes(2)
+						.jlptLevel("N5")
+						.jyoyoGradeTaught(1)
+						.mostUsedInNewspapers(5)
+						.radicals(List.of("人"))
+						.build(),
+				Kanji.builder()
+						.kanji("空")
+						.meanings(List.of("empty", "sky", "void", "vacant", "vacuum"))
+						.kunyomi(List.of("そら", "あ.く", "あ.き", "あ.ける", "から", "す.く", "す.かす", "むな.しい"))
+						.kunyomi_romaji(List.of("sora", "a.ku", "a.ki", "a.keru", "kara", "su.ku", "su.kasu", "muna.shii"))
+						.onyomi(List.of("クウ"))
+						.onyomi_romaji(List.of("kuu"))
+						.strokes(8)
+						.jlptLevel("N4")
+						.jyoyoGradeTaught(1)
+						.mostUsedInNewspapers(304)
+						.radicals(List.of("儿", "宀", "工", "穴"))
+						.build()
+		);
+		kanjiRepository.saveAll(testData);
+
+		given()
+				.contentType(ContentType.JSON)
+				.when()
+				.get("api/v1/kanji")
+				.then()
+				.statusCode(200)
+				.body(".", hasSize(2));
 	}
 
 }
