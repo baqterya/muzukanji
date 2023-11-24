a722d38003a55edfb428e8ebee0711514f0260b3
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-24 13:02:05 +0100

    created kanji data class

---
+++ src/main/java/com/baqterya/muzukanji/model/Kanji.java
@@ -0,0 +1,48 @@
+package com.baqterya.muzukanji.model;
+
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "kanji_table")
+public class Kanji {
+
+    @Id
+    @SequenceGenerator(
+            name = "kanji_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Integer id;
+    private String kanji;
+    @Type(ListArrayType.class)
+    private List<String> meanings;
+    @Type(ListArrayType.class)
+    private List<String> kunyomi;
+    @Type(ListArrayType.class)
+    private List<String> onyomi;
+    private Integer strokes;
+    private String jlptLevel;
+    private Integer jyoyoGradeTaught;
+    private Integer mostUsedInNewspapers;
+    @Type(ListArrayType.class)
+    private List<String> parts;
+    private String radical;
+
+}
