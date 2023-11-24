28a821b83184f629c16c2c2e19a9befee2985091
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-24 15:07:03 +0100

    created kanji service

---
+++ src/main/java/com/baqterya/muzukanji/model/KanjiService.java
@@ -0,0 +1,28 @@
+package com.baqterya.muzukanji.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class KanjiService {
+
+    private final KanjiRepository kanjiRepository;
+
+    public List<Kanji> getAllKanji() {
+        return kanjiRepository.findAll();
+    }
+
+    public void addNewKanji(Kanji newKanji) {
+        Optional<Kanji> kanjiById = kanjiRepository.findById(newKanji.getId());
+        if (kanjiById.isPresent()) {
+            throw new IllegalStateException(String.format(
+                    "Kanji %s already in the database", newKanji.getKanji()
+            ));
+        }
+        kanjiRepository.save(newKanji);
+    }
+}
