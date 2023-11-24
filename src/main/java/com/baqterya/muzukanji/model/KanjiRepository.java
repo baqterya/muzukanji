ca24037060d6931ee84ce95626bed4a2488393dc
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-24 15:06:50 +0100

    created kanji repository interface

---
+++ src/main/java/com/baqterya/muzukanji/model/KanjiRepository.java
@@ -0,0 +1,23 @@
+package com.baqterya.muzukanji.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface KanjiRepository extends JpaRepository<Kanji, Integer> {
+// TODO make paginable in the future
+
+    Optional<Kanji> findByKanji(String kanji);
+    List<Optional<Kanji>> findByMeaning(String meaning);
+    List<Optional<Kanji>> findByReadingKana(String reading);
+    List<Optional<Kanji>> findByReadingRomaji(String reading);
+    List<Optional<Kanji>> findByJlptLevel(String level);
+    List<Optional<Kanji>> findByStrokes(Integer strokes);
+    List<Optional<Kanji>> findByJyoyo(Integer grade);
+    List<Optional<Kanji>> findByUsage(Integer usage);
+    List<Optional<Kanji>> findByRadicals(List<String> radicals);
+
+}
