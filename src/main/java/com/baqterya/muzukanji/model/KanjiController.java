bb199130b7a3bfa421b47a4e67b0ce3bc88714c4
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-24 15:07:30 +0100

    created kanji controller

---
+++ src/main/java/com/baqterya/muzukanji/model/KanjiController.java
@@ -0,0 +1,19 @@
+package com.baqterya.muzukanji.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/kanji")
+@RequiredArgsConstructor
+public class KanjiController {
+
+    private final KanjiService kanjiService;
+
+    public List<Kanji> getAllKanji() {
+        return kanjiService.getAllKanji();
+    }
+}
