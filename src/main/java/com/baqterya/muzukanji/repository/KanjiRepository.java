77f184fd090622b76623e7d13129dbd3f58af8d9
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-29 17:23:34 +0100

    changed findAll to a paginated method

--- src/main/java/com/baqterya/muzukanji/repository/KanjiRepository.java
+++ src/main/java/com/baqterya/muzukanji/repository/KanjiRepository.java
@@ -1,7 +1,10 @@
 package com.baqterya.muzukanji.repository;
 
 import com.baqterya.muzukanji.model.Kanji;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +12,9 @@ import java.util.Optional;
 
 @Repository
 public interface KanjiRepository extends JpaRepository<Kanji, Integer> {
-// TODO make paginable in the future
+
+    @Override
+    @NonNull Page<Kanji> findAll(@NonNull Pageable pageable);
 
     Optional<Kanji> findByKanji(String kanji);
 
