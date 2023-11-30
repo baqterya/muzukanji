00d13f62433be7a23ca17fe7c19344ba8be48865
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-11-30 18:09:42 +0100

    created kanji model assembler for hateoas pagination

---
+++ src/main/java/com/baqterya/muzukanji/model/KanjiModelAssembler.java
@@ -0,0 +1,23 @@
+package com.baqterya.muzukanji.model;
+
+import com.baqterya.muzukanji.controller.KanjiController;
+import com.baqterya.muzukanji.model.Kanji;
+import com.baqterya.muzukanji.model.KanjiModel;
+import lombok.NonNull;
+import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KanjiModelAssembler extends RepresentationModelAssemblerSupport<Kanji, KanjiModel> {
+    public KanjiModelAssembler() {
+        super(KanjiController.class, KanjiModel.class);
+    }
+
+    @Override
+    @NonNull public KanjiModel toModel(@NonNull Kanji entity) {
+        KanjiModel kanjiModel = new KanjiModel();
+        BeanUtils.copyProperties(entity, kanjiModel);
+        return kanjiModel;
+    }
+}
