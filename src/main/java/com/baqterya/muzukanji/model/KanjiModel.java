a8ca43d423e8201fb144463410d6c2c11f7d65cc
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-12-01 12:44:51 +0100

    removed unnecessary fields from KanjiModel

--- src/main/java/com/baqterya/muzukanji/model/KanjiModel.java
+++ src/main/java/com/baqterya/muzukanji/model/KanjiModel.java
@@ -17,12 +17,4 @@ public class KanjiModel extends RepresentationModel<KanjiModel> {
     private Integer id;
     private String kanji;
     private List<String> meanings;
-    private List<String> kunyomi;
-    private List<String> kunyomi_romaji;
-    private List<String> onyomi;
-    private List<String> onyomi_romaji;
-    private Integer strokes;
-    private String jlptLevel;
-    private Integer jyoyoGradeTaught;
-    private Integer mostUsedInNewspapers;
 }
