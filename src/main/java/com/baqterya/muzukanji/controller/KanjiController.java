c22625f85e67caa3625528fd66bb041cf766b340
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-12-01 12:54:20 +0100

    added mapping for getKanjiById and changed the items displayed in getAllKanji to model with HATEOAS links to full data in getKanjiById

--- src/main/java/com/baqterya/muzukanji/controller/KanjiController.java
+++ src/main/java/com/baqterya/muzukanji/controller/KanjiController.java
@@ -12,15 +12,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,8 +51,7 @@ public class KanjiController {
             Page<Kanji> kanjiPage = kanjiService.getAllKanji(pagingSort);
             PagedModel<KanjiModel> kanjiPagedModel = pagedResourcesAssembler.toModel(kanjiPage, kanjiModelAssembler);
 
-
-            List<Kanji> kanjiList = kanjiPage.getContent();
+            Collection<KanjiModel> kanjiList = kanjiPagedModel.getContent();
             if (kanjiList.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -75,6 +74,14 @@ public class KanjiController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll")
+    public Kanji getKanjiById(
+            @PathVariable Integer id
+    ) {
+        return kanjiService.getKanjiById(id);
     }
 
     @GetMapping("/admin")
