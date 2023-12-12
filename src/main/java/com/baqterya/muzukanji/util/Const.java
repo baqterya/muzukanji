14fd81caf65e4caca8368cb0b59df03f79398c17
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-12-12 14:04:54 +0100

    added "kanji already exists" message

--- src/main/java/com/baqterya/muzukanji/util/Const.java
+++ src/main/java/com/baqterya/muzukanji/util/Const.java
@@ -2,4 +2,5 @@ package com.baqterya.muzukanji.util;
 
 public class Const {
     public static final String KANJI_NOT_FOUND_BY_ID_MESSAGE = "Kanji with id %d not found in the database";
+    public static final String KANJI_ALREADY_EXISTS_MESSAGE = "Kanji '%s' already exists";
 }
