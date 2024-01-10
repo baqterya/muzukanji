8f4b1c9a9083682bd5b030804a30ebd5d168ca5b
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-01-10 22:17:13 +0100

    added const regexp of acceptable jlpt values

--- src/main/java/com/baqterya/muzukanji/util/Const.java
+++ src/main/java/com/baqterya/muzukanji/util/Const.java
@@ -3,4 +3,6 @@ package com.baqterya.muzukanji.util;
 public class Const {
     public static final String KANJI_NOT_FOUND_BY_ID_MESSAGE = "Kanji with id %d not found in the database";
     public static final String KANJI_ALREADY_EXISTS_MESSAGE = "Kanji '%s' already exists";
+
+    public static final String JLPT_REGEX = "^(N5|N4|N3|N2|N1|n5|n4|n3|n2|n1)$";
 }
