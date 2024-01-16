0752138074b5ab0819ccec67329a187999cd52dc
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-01-16 17:02:39 +0100

    added TEST_KANJI const value for usage in tests

--- src/main/java/com/baqterya/muzukanji/util/Const.java
+++ src/main/java/com/baqterya/muzukanji/util/Const.java
@@ -1,5 +1,7 @@
 package com.baqterya.muzukanji.util;
 
+import com.baqterya.muzukanji.model.Kanji;
+
 public class Const {
     public static final String KANJI_NOT_FOUND_BY_ID_MESSAGE = "Kanji with id %d not found in the database";
     public static final String KANJI_ALREADY_EXISTS_MESSAGE = "Kanji '%s' already exists";
@@ -7,4 +9,11 @@ public class Const {
     public static final String JLPT_REGEX = "^(N5|N4|N3|N2|N1|n5|n4|n3|n2|n1)$";
     public static final String JLPT_ERROR_MESSAGE
             = "Invalid JLPT level entered. JLPT level must match (N5|N4|N3|N2|N1|n5|n4|n3|n2|n1)" ;
+
+    public static final Kanji TEST_KANJI = new Kanji(
+            "一", "One, One Radical (no.1)",
+            "ひと-, ひと.つ", "hito-, hito.tsu",
+            "イチ, イツ", "ichi, itsu",
+            1, "N5", 1, 2
+    );
 }
