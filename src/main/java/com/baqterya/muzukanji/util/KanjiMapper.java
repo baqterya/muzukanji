1c84a8374965eb31a06ca6aad0f77bd894a30f6c
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-01-18 14:11:30 +0100

    added KanjiDto to kanji mapping

--- src/main/java/com/baqterya/muzukanji/util/KanjiMapper.java
+++ src/main/java/com/baqterya/muzukanji/util/KanjiMapper.java
@@ -2,13 +2,17 @@ package com.baqterya.muzukanji.util;
 
 import com.baqterya.muzukanji.model.Kanji;
 import com.baqterya.muzukanji.model.KanjiDto;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface KanjiMapper {
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
     void updateKanjiFromDto(KanjiDto dto, @MappingTarget Kanji kanji);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    Kanji kanjiDtoToKanji(KanjiDto dto);
+
 }
