ab5129ec623f1b2dcb3206790a9fd49ae37705d5
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-01-18 14:12:46 +0100

    properly defined security config for all endpoints

--- src/main/java/com/baqterya/muzukanji/security/SecurityConfiguration.java
+++ src/main/java/com/baqterya/muzukanji/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.baqterya.muzukanji.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,21 +21,26 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String apiUri = "api/v1/kanji";
+        String idApiUri = "api/v1/kanji/{kanjiId}";
         return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers("api/v1/kanji").permitAll()
-                                .requestMatchers("api/v1/kanji/{id}").permitAll()
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                auth -> auth
+                    .requestMatchers(HttpMethod.GET, apiUri).permitAll()
+                    .requestMatchers(HttpMethod.GET, idApiUri).permitAll()
+                    .requestMatchers(HttpMethod.POST, apiUri).authenticated()
+                    .requestMatchers(HttpMethod.PUT, idApiUri).authenticated()
+                    .requestMatchers(HttpMethod.DELETE, idApiUri).authenticated()
+            )
+            .oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(
+                    jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
                 )
-                .oauth2ResourceServer(
-                        oauth2 -> oauth2.jwt(
-                                jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
-                        )
-                )
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .build();
+            )
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .build();
     }
 }
