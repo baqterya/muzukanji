99d499ba03cf0455b0c1043198fd926ba76463f3
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-01-19 14:41:04 +0100

    added TEST_KANJI_PAYLOAD for testing POST

--- src/main/java/com/baqterya/muzukanji/util/Const.java
+++ src/main/java/com/baqterya/muzukanji/util/Const.java
@@ -49,4 +49,21 @@ public class Const {
             .jyoyoGradeTaught(1)
             .mostUsedInNewspapers(2)
             .build();
+
+    public static String TEST_KANJI_PAYLOAD =
+            """
+                {
+                   "kanji": "一",
+                   "meanings": "One, One Radical (no.1)",
+                   "kunyomi": "ひと-, ひと.つ",
+                   "kunyomiRomaji": "hito-, hito.tsu",
+                   "onyomi": "イチ, イツ",
+                   "onyomiRomaji": "ichi, itsu",
+                   "strokes": 1,
+                   "jlptLevel": "N5",
+                   "jyoyoGradeTaught": 1,
+                   "mostUsedInNewspapers": 2
+                }
+            """;
+
 }
